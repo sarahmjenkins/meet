535ef2f119486d130c2eee402f82b7a2428a4d68
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-10-08 17:01:50 -0600

    Tests for initial set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import { extractLocations } from './api';
+import { mockData } from './mock-data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    locations: extractLocations(mockData),
+    events: mockData,
+  }
+  
+  render() {
+    const { locations, events } = this.state;
+
+    return (
+      <div className="App">
+        <CitySearch locations={locations} />
+        <EventList events={events}/>
+      </div>
+    );
+  }
 }
 
 export default App;
