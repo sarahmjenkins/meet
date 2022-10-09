8f6766531d7476fd6bc7b6f63a8088e8a0c9b8a5
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-10-09 15:05:53 -0600

    Added component and testing

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,30 @@
+import React, { Component} from 'react';
+
+class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 32
+  };
+
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      numberOfEvents: value
+    });
+  };
+
+  render() {
+    return (
+      <div className="event-number">
+        <input 
+          type="text"
+          className="display-number"
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChange}
+        />
+
+      </div>
+    )
+  }
+}
+
+export default NumberOfEvents;
---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+// Tests to include:
+//   (x)Render text input
+//   (x)Input default is 32
+//   (x)Text input changes state
+//   (x)Text input is rendered correctly
+//   ( )Text input matches number of events rendered ()
+
+describe('<NumberOfEvents /> component', () => {
+  test('render text input', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    expect(NumberOfEventsWrapper.find('input')).toHaveLength(1);
+  });
+
+  test('default input is 32', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    expect(NumberOfEventsWrapper.find('input').prop('value')).toBe(32);
+  });
+
+  test('text input changes value of number of events', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    NumberOfEventsWrapper.setState({
+      numberOfEvents: 32
+    })
+    const inputEvent = { target: { value: 5 }};
+    NumberOfEventsWrapper.find('input').simulate('change', inputEvent);
+    expect (NumberOfEventsWrapper.state('numberOfEvents')).toBe(5);
+  })
+
+  test('text input rendered correctly', () => {
+    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents')
+    expect(NumberOfEventsWrapper.find('input').prop('value')).toBe(numberOfEvents);
+  })
+
+})
