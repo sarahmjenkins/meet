7ba0eceb7b50bde89c720b6e6220e89baac86fd8
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-10-21 14:50:30 -0600

    Added location and time to events

--- src/Event.js
+++ src/Event.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
 
 class Event extends Component {
   state = {
@@ -13,13 +14,22 @@ class Event extends Component {
   
   render() {
     const { event } = this.props;
+    const startTime = this.props.event.start.dateTime;
+    const endTime = this.props.event.end.dateTime;
 
     return (
       <div className="event">
         <h1 className="title">{event.summary}</h1>
+        <h3 className="location">{event.location}</h3>
         <div className="details-container">
-          {this.state.isVisible && <p className="event-details">{event.description}</p>}
-          {this.state.isVisible ? <button className="toggle-button" onClick={this.handleClick}>Hide Details</button>
+          {this.state.isVisible 
+            && <div className="event-details">
+              <p><b>About this event:</b> {event.description}</p>
+              <p><b>Start:</b> <Moment>{startTime}</Moment></p>
+              <p><b>End:</b> <Moment>{endTime}</Moment></p>
+            </div>}
+          {this.state.isVisible 
+            ? <button className="toggle-button" onClick={this.handleClick}>Hide Details</button>
             : <button className="toggle-button" onClick={this.handleClick}>Get Details</button>
           } 
         </div>
