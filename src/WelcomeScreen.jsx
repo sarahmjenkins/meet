363ceab34925c968f07c8ac48c3eaf4e739f8229
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-10-26 13:33:55 -0600

    WelcomeScreen styling

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -5,24 +5,28 @@ function WelcomeScreen(props) {
   return props.showWelcomeScreen ?
   (
       <div className="WelcomeScreen">
-        <h1>Welcome to the Meet app</h1>
-        <h4>Log in to see upcoming events around the world for full-stack developers</h4>
-        <div className="button_cont" align="center">
-          <div clasName="google-btn">
-            <div className="google-icon-wrapper">
-              <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google sign-in" />
+        <div className="content">
+          <h1>Meet App</h1>
+          <h4>Log in to see upcoming events around the world for full-stack developers.</h4>
+          <div className="button_cont" align="center">
+            <div className="google-btn">
+              <div className="google-icon-wrapper">
+                <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google sign-in" />
+              </div>
+              <button className="btn-text" onClick={() => { props.getAccessToken() }} rel="nofollow noopener">
+                <b>Sign in with Google</b>
+              </button>
             </div>
-            <button onClick={() => { props.getAccessToken() }} rel="nofollow noopener" className="btn-text">
-              <b>Sign in with Google</b>
-            </button>
+          </div>
+          <div className="verification-links">
+            <a className="oauth-homepage" href="https://sarahmjenkins.github.io/meet/homepage.html">
+              Google OAuth2 Homepage
+            </a>
+            <a className="privacy-policy" href="https://sarahmjenkins.github.io/meet/privacy.html" rel="nofollow noopener">
+              Privacy Policy
+            </a>
           </div>
         </div>
-        <a className="oauth-homepage" href="https://sarahmjenkins.github.io/meet/homepage.html">
-          Google OAuth2 Homepage
-        </a>
-        <a className="privacy-policy" href="https://sarahmjenkins.github.io/meet/privacy.html" rel="nofollow noopener">
-          Privacy Policy
-        </a>
       </div>
   )
   : null
