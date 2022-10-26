ed2d0ef76b63ede1aff9799c5f336e30dd009865
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-10-25 18:10:01 -0600

    Fixed error

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
     const {locations, events} = this.state;
     const data = locations.map((location) => {
       const number = events.filter((event) => event.location === location).length;
-      const city = location.split(', ').split(' - ').shift();
+      const city = location.split(', ').shift();
       return {city, number};
     });
     return data;
